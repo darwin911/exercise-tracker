e228b2305f1a9e4f0ba51aaf5d8f43fd49127b86
Darwin Smith <darwin911@gmail.com>
2020-01-29 13:37:06 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+// current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version. To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('*** MongoDB database connection established successfully ***');
+});
+
+app.listen(port, () => {
+  console.log(`Server is runniong on port: ${port}`);
+});
