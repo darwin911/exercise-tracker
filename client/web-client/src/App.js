8bf1a39e7a7d488217fcfe4f456f97ff494e7743
Darwin Smith <darwin911@gmail.com>
2020-02-04 17:03:56 -0500

    adds fetch test call, loads into state and renders list of exercises

--- client/web-client/src/App.js
+++ client/web-client/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [exercises, setExercises] = useState([]);
+
+  const loadExercises = () => {
+    fetch('http://localhost:5000/exercises')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setExercises(data);
+      });
+  };
+
+  useEffect(() => {
+    loadExercises();
+    return () => setExercises([]);
+  }, [setExercises]);
+
   return (
     <div className='App'>
       <h1>Exercise Tracker</h1>
+      {exercises.map(exercise => (
+        <p key={exercise}>{JSON.stringify(exercise)}</p>
+      ))}
     </div>
   );
 }
