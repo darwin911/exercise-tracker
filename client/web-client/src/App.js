fc1653f8b97eecbfaeb28a9d4eca8b30521c2dbb
Darwin Smith <darwin911@gmail.com>
2020-02-05 16:03:59 -0500

    abstracts AddExercise into own component + moves handleDelete to Exercise.js

--- client/web-client/src/App.js
+++ client/web-client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Exercise } from './components/Exercise';
+import { AddExercise } from './components/AddExercise';
 import { getExercises } from './helper';
 
 function App() {
@@ -15,50 +16,14 @@ function App() {
     loadExercises();
   }, [setExercises]);
 
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('http://localhost:5000/exercises/add', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        duration: 5,
-        note: 'test note',
-        date: new Date(),
-        username: 'Darwin'
-      })
-    });
-    console.log(await response.json());
-  };
-
-  const handleDelete = async id => {
-    const response = await fetch(`http://localhost:5000/exercises/${id}`, {
-      method: 'DELETE'
-    });
-    console.log(await response.json());
-  };
-
   return (
     <div className='App'>
       <h1>Exercise Tracker</h1>
       {exercises.map(exercise => (
-        <Exercise
-          key={exercise._id}
-          exercise={exercise}
-          handleDelete={handleDelete}
-        />
+        <Exercise key={exercise._id} exercise={exercise} />
       ))}
 
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='duration'>Duration: </label>
-        <input type='number' name='duration' max={360} />
-        <br />
-        <label htmlFor='note'>Note: </label>
-        <input type='text' name='note' />
-        <br />
-        <button>Add Exercise</button>
-      </form>
+      <AddExercise />
     </div>
   );
 }
--- client/web-client/src/components/Exercise.js
+++ client/web-client/src/components/Exercise.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-export const Exercise = ({ exercise, handleDelete }) => {
+export const Exercise = ({ exercise }) => {
   const { _id, username, note, duration, date } = exercise;
+
+  const handleDelete = async id => {
+    const response = await fetch(`http://localhost:5000/exercises/${id}`, {
+      method: 'DELETE'
+    });
+    console.log(await response.json());
+  };
 
   return (
     <div>
