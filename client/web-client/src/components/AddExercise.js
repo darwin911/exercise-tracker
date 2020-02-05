421c7720e0e763397bcfc0ef29cc192402e143a9
Darwin Smith <darwin911@gmail.com>
2020-02-05 16:18:03 -0500

    refactors to connect form with duration and note

--- client/web-client/src/components/AddExercise.js
+++ client/web-client/src/components/AddExercise.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { addExercise } from '../helper';
 
 export const AddExercise = () => {
+  const [duration, setDuration] = useState(null);
+  const [note, setNote] = useState('');
+
   const handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/exercises/add', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        duration: 5,
-        note: 'test note',
-        date: new Date(),
-        username: 'Darwin'
-      })
-    });
-    console.log(await response.json());
+
+    const obj = {
+      duration,
+      note,
+      date: new Date(),
+      username: 'Darwin'
+    };
+
+    const resp = await addExercise(obj);
+    console.log(resp);
   };
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor='duration'>Duration: </label>
-      <input type='number' name='duration' max={360} />
+      <input
+        type='number'
+        name='duration'
+        max={360}
+        onChange={e => setDuration(e.target.value)}
+      />
       <br />
       <label htmlFor='note'>Note: </label>
-      <input type='text' name='note' />
+      <input type='text' name='note' onChange={e => setNote(e.target.value)} />
       <br />
       <button>Add Exercise</button>
     </form>
