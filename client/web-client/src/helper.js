8c1d40e022b5082393381714be4e20610c67e6fb
Darwin Smith <darwin911@gmail.com>
2020-02-05 16:10:55 -0500

    adds BASE_URL to build api call

--- client/web-client/src/helper.js
+++ client/web-client/src/helper.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
 
+const BASE_URL = 'http://localhost:5000';
+
 const getExercises = async () => {
   try {
-    const resp = await axios.get('http://localhost:5000/exercises');
+    const resp = await axios.get(`${BASE_URL}/exercises`);
     return resp.data;
   } catch (err) {
     console.error(err);
   }
 };
 
-export { getExercises };
+const deleteExercise = async id => {
+  try {
+    const resp = axios.delete(`${BASE_URL}/exercises/${id}`);
+    return resp.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export { getExercises, deleteExercise };
