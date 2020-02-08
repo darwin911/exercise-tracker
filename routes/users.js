6d45a379304059d4d596671b5a9e59a013e012ac
Darwin Smith <darwin911@gmail.com>
2020-02-08 13:09:44 -0500

    updates routes with async/await and try/catch

--- routes/users.js
+++ routes/users.js
@@ -2,53 +2,66 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 const { verify } = require('../auth');
 
-router.route('/').get((req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route('/').get(async (req, res) => {
+  try {
+    let users = await User.find();
+    users = await Promise.all(users.map(user => user.toClient()));
+    res.json({ users });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.route('/:id').get((req, res) => {
-  User.findById(req.params.id)
-    .then(user => res.json(user))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route('/:id').get(async (req, res) => {
+  try {
+    let user = await User.findById(req.params.id);
+    user = await user.toClient();
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.route('/login').post(async (req, res) => {
   const { email, password } = req.body;
-  let user = await User.findOne({ email });
-  if (user) {
-    const isAuthenticated = await user.isValidPassword(password);
-    if (isAuthenticated) {
-      const userData = await user.toJSON();
-      return res.json(userData);
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      const isAuthenticated = await user.isValidPassword(password);
+      if (isAuthenticated) {
+        const userData = await user.toAuthJSON();
+        return res.json(userData);
+      }
+      res.status(401).json({ error: 'Invalid Credentials' });
     }
-    res.status(401).json({ error: 'Invalid Credentials' });
+  } catch (error) {
+    console.log(error);
   }
 });
 
 router.route('/register').post(async (req, res) => {
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = req.body.password;
+  const { username, email, password } = req.body;
+  try {
+    const newUser = new User({ username, email });
+    await newUser.setPassword(password);
 
-  const newUser = new User({ username, email });
-  await newUser.setPassword(password);
-
-  const userData = await newUser.toJSON();
-  newUser
-    .save()
-    .then(() => res.json(userData))
-    .catch(err => res.status(400).json('Error: ' + err));
+    const userData = await newUser.toAuthJSON();
+    await newUser.save();
+    return res.json(userData);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.route('/verify').post(async (req, res) => {
-  let token = req.body.token;
-  const verifiedData = await verify(token);
-  const userId = verifiedData.id;
-  const user = await User.findById(userId);
-  const userData = await user.toProfileJSON();
-  res.json(userData);
+  try {
+    const { id } = await verify(req.body.token);
+    const user = await User.findById(id);
+    const userData = await user.toProfileJSON();
+    res.json(userData);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
