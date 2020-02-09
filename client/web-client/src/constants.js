6b053da5f15d039a594cae2cfd3d585a25fc4a35
Darwin Smith <darwin911@gmail.com>
2020-02-09 09:05:00 -0500

    adds Exercise constants

--- client/web-client/src/constants.js
+++ client/web-client/src/constants.js
@@ -1,6 +1,9 @@
 export const LOGIN = 'LOGIN';
 export const REGISTER = 'REGISTER';
 export const SET_USER = 'SET_USER';
+export const SET_EXERCISES = 'SET_EXERCISES';
+export const ADD_EXERCISE = 'ADD_EXERCISE';
+export const REMOVE_EXERCISE = 'REMOVE_EXERCISE';
 export const CLEAR_USER = 'CLEAR_USER';
 export const LOGOUT = 'LOGOUT';
 export const LOADING = 'LOADING_USER';
