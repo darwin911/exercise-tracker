7d015cb89b2db24d7cccd9a22578e4cd565778f5
Darwin Smith <darwin911@gmail.com>
2020-02-09 10:56:43 -0500

    adds loader to UserExercises

--- client/web-client/src/components/UserExercises.js
+++ client/web-client/src/components/UserExercises.js
@@ -5,16 +5,20 @@ import { AuthContext } from '../Store';
 
 export const UserExercises = () => {
   const state = useContext(AuthContext)[0];
-  const { user, exercises } = state;
+  const { user, exercises, loading } = state;
 
-  return (
-    <div className='user-exercises'>
-      {user && <h4>{user ? user.username : 'Guest'}'s Exercises</h4>}
-      {exercises.map(exercise => (
-        <Exercise key={exercise._id} exercise={exercise} />
-      ))}
-      <hr />
-      <AddExercise />
-    </div>
-  );
+  if (!loading) {
+    return (
+      <div className='user-exercises'>
+        {user && <h4>{user ? user.username : 'Guest'}'s Exercises</h4>}
+        {exercises.map(exercise => (
+          <Exercise key={exercise._id} exercise={exercise} />
+        ))}
+        <hr />
+        <AddExercise />
+      </div>
+    );
+  } else {
+    return <div className='loader'></div>;
+  }
 };
