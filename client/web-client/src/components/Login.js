e5ca392ff39a758c74d131478617f77484611018
Darwin Smith <darwin911@gmail.com>
2020-02-13 13:37:43 -0500

    Adds error handling for Login Component

--- client/web-client/src/components/Login.js
+++ client/web-client/src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { loginUser } from '../helper';
 import { Link, useHistory } from 'react-router-dom';
 import { AuthContext } from '../Store';
-import { SET_USER, LOADING } from '../constants';
+import { SET_USER, TOGGLE_LOADING } from '../constants';
 
 export const Login = () => {
   const [state, dispatch] = useContext(AuthContext);
@@ -10,18 +10,26 @@ export const Login = () => {
   let history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
   const handleLogin = async e => {
     e.preventDefault();
-    dispatch({ type: LOADING });
+    dispatch({ type: TOGGLE_LOADING });
 
     const authenticatedUser = await loginUser({ email, password });
+
+    if (authenticatedUser.error) {
+      console.log(authenticatedUser);
+      setError(authenticatedUser.error);
+      dispatch({ type: TOGGLE_LOADING });
+      return;
+    }
 
     if (authenticatedUser) {
       dispatch({ type: SET_USER, payload: authenticatedUser });
       localStorage.setItem('token', authenticatedUser.token);
+      history.push('/');
     }
-    history.push('/');
   };
 
   return (
@@ -56,6 +64,7 @@ export const Login = () => {
           <button disabled={state.loading}>
             {state.loading ? <div className='loader' /> : 'Submit'}
           </button>
+          {error && <p className='error'>{error}</p>}
         </form>
         <p>
           Don't have an account? <Link to='/register'>Register</Link>
