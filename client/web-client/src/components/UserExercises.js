50b109aa21f0277395c955938e2590267aaf8c98
Darwin Smith <darwin911@gmail.com>
2020-02-14 19:31:17 -0500

    renders exercises or filteredExercises

--- client/web-client/src/components/UserExercises.js
+++ client/web-client/src/components/UserExercises.js
@@ -1,13 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { TOGGLE_MODAL } from '../constants';
-import { Exercise } from './Exercise';
 import { ExercisesSummary } from './ExercisesSummary';
+import { ExerciseList } from './ExerciseList';
 import { AuthContext } from '../Store';
 import { AnimatePresence } from 'framer-motion';
 
 export const UserExercises = () => {
   const [state, dispatch] = useContext(AuthContext);
-  const { user, exercises, loading } = state;
+  const { user, exercises, loading, filteredExercises } = state;
   const [totalMins, setTotalMins] = useState(0);
 
   useEffect(() => {
@@ -32,9 +32,7 @@ export const UserExercises = () => {
 
         <div className='exercises__container'>
           <AnimatePresence>
-            {exercises.map(exercise => (
-              <Exercise key={exercise.id} exercise={exercise} />
-            ))}
+            <ExerciseList exercises={filteredExercises || exercises} />
           </AnimatePresence>
           <div style={{ gridColumn: '1 / -1' }}>
             <button className='btn toggle-form' onClick={toggleModal}>
