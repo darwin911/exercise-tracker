f5a606099e36576c5471b979dc56e122d0002b04
Darwin Smith <darwin911@gmail.com>
2020-02-14 19:30:47 -0500

    creates FilterExercises

---
+++ client/web-client/src/components/FilterExercises.js
@@ -0,0 +1,28 @@
+import React, { useState, useContext } from 'react';
+import {
+  FILTER_ALL,
+  FILTER_CURRENT_MONTH,
+  FILTER_CURRENT_WEEK
+} from '../constants';
+import { AuthContext } from '../Store';
+
+export const FilterExercises = () => {
+  const [state, dispatch] = useContext(AuthContext);
+
+  const [filterOption, setFilterOption] = useState('All');
+
+  const handleChange = e => {
+    const { value } = e.target;
+    setFilterOption(value);
+    console.log(value);
+    dispatch({ type: value });
+  };
+
+  return (
+    <select className='filter' value={filterOption} onChange={handleChange}>
+      <option value={FILTER_ALL}>All</option>
+      <option value={FILTER_CURRENT_WEEK}>This Week</option>
+      <option value={FILTER_CURRENT_MONTH}>This Month</option>
+    </select>
+  );
+};
