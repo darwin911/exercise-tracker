7fd7e87df4c93851005e6dd8e71e3e9af73232da
Darwin Smith <darwin911@gmail.com>
2020-02-17 18:15:12 -0500

    adds exercise count and total mins to initialState

--- client/web-client/src/Store.js
+++ client/web-client/src/Store.js
@@ -5,7 +5,9 @@ const initialState = {
   user: null,
   loading: false,
   exercises: [],
-  modalOpen: false
+  exerciseCount: 0,
+  exerciseMins: 0,
+  modalOpen: false,
 };
 
 export const AuthContext = createContext(initialState);
@@ -13,9 +15,5 @@ export const AuthContext = createContext(initialState);
 export const Store = ({ children }) => {
   const [state, dispatch] = useReducer(Reducer, initialState);
 
-  return (
-    <AuthContext.Provider value={[state, dispatch]}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={[state, dispatch]}>{children}</AuthContext.Provider>;
 };
