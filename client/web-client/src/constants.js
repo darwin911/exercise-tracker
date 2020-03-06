1e815880e5196dee86e8bf783313ecb5f553c1ad
Darwin Smith <darwin911@gmail.com>
2020-03-06 15:36:52 -0500

    adds edit exercise constant

--- client/web-client/src/constants.js
+++ client/web-client/src/constants.js
@@ -4,6 +4,7 @@ export const SET_USER = 'SET_USER';
 export const SET_EXERCISES = 'SET_EXERCISES';
 export const ADD_EXERCISE = 'ADD_EXERCISE';
 export const REMOVE_EXERCISE = 'REMOVE_EXERCISE';
+export const EDIT_EXERCISE = 'EDIT_EXERCISE';
 export const CLEAR_USER = 'CLEAR_USER';
 export const LOGOUT = 'LOGOUT';
 export const TOGGLE_LOADING = 'TOGGLE_LOADING';
