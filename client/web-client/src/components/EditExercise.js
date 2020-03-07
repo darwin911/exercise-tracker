033a1cc120257c9299da596736957f02963c05a6
Darwin Smith <darwin911@gmail.com>
2020-03-07 15:09:14 -0500

    creates EditExercise

---
+++ client/web-client/src/components/EditExercise.js
@@ -0,0 +1,164 @@
+import React, { useState, useContext } from 'react';
+import { editExercise } from '../helper';
+import { AuthContext } from '../Store';
+import { CLEAR_EDIT_EXERCISE, UPDATE_EXERCISE } from '../constants';
+import { motion } from 'framer-motion';
+import { createPortal } from 'react-dom';
+import moment from 'moment';
+
+export const EditExercise = ({ exercise }) => {
+  const [state, dispatch] = useContext(AuthContext);
+  const { user } = state;
+
+  const [distance, setDistance] = useState(exercise.distance); // Units in miles (imperial)
+  const [date, setDate] = useState(moment(exercise.date).format(moment.HTML5_FMT.DATE));
+  const [time, setTime] = useState(moment().format(moment.HTML5_FMT.TIME));
+  const [duration, setDuration] = useState(exercise.duration);
+  const [note, setNote] = useState(exercise.note);
+
+  const [loading, setLoading] = useState(false);
+
+  const handleEdit = async e => {
+    e.preventDefault();
+    setLoading(true);
+
+    // if (duration < 1) {
+    //   setLoading(false);
+    //   return;
+    // }
+
+    let exerciseObj = {
+      duration,
+      userId: user.id,
+      note,
+      date,
+      username: user.username,
+      type: exercise.type,
+      distance,
+    };
+    const updatedExercise = await editExercise(exercise.id, exerciseObj);
+    // debugger;
+
+    if (updatedExercise) {
+      dispatch({ type: UPDATE_EXERCISE, payload: updatedExercise });
+    }
+
+    // dispatch({ type: TOGGLE_MODAL });
+    // resetForm();
+  };
+
+  const resetForm = () => {
+    setLoading(false);
+    setDate(moment().format(moment.HTML5_FMT.DATE));
+    setDuration('');
+    setNote('');
+    setDistance('');
+  };
+
+  const handleClose = () => {
+    dispatch({ type: CLEAR_EDIT_EXERCISE });
+    resetForm();
+  };
+
+  const dateField = (
+    <div className='form-field date'>
+      <label htmlFor='date'>Date:</label>
+      <input
+        id='date'
+        type='date'
+        pattern='\d{4}-\d{2}-\d{2}'
+        onChange={e => setDate(e.target.value)}
+        value={date}
+        required
+      />
+    </div>
+  );
+
+  const timeField = (
+    <div className='form-field time'>
+      <label htmlFor='time'>Time:</label>
+      <input id='time' type='time' onChange={e => setTime(e.target.value)} value={time} required />
+    </div>
+  );
+
+  const durationField = (
+    <div className='form-field duration'>
+      <label htmlFor='duration'>Duration: </label>
+      <input
+        id='duration'
+        type='number'
+        inputMode='numeric'
+        placeholder='0'
+        min={1}
+        max={360}
+        onChange={e => setDuration(e.target.value)}
+        required
+        value={duration}
+      />
+      <label htmlFor='duration'>min{duration > 1 && 's'}</label>
+    </div>
+  );
+
+  const noteField = (
+    <div className='form-field note'>
+      <label htmlFor='note'>Note: </label>
+      <input
+        id='note'
+        type='text'
+        placeholder='Felt great!'
+        onChange={e => setNote(e.target.value)}
+        required
+        value={note}
+      />
+    </div>
+  );
+
+  const distanceField = exercise.distance ? (
+    <div className='form-field distance'>
+      <label htmlFor='note'>Distance: </label>
+      <input
+        id='distance'
+        type='number'
+        placeholder='0.0'
+        step={0.1}
+        min={0.1}
+        onChange={e => setDistance(e.target.value)}
+        value={distance}
+        required
+      />
+      <label htmlFor='distance'>mi</label>
+    </div>
+  ) : null;
+
+  const buttonsContainer = (
+    <div className='form-field buttons-container'>
+      <button className='btn edit' onClick={e => handleEdit(e)} disabled={loading || !duration}>
+        {loading ? <div className='loader' /> : 'Save'}
+      </button>
+      <button type='button' className='btn cancel' onClick={() => handleClose()} disabled={loading}>
+        Cancel
+      </button>
+    </div>
+  );
+
+  if (exercise) {
+    // modalOpen
+    return createPortal(
+      <aside className='edit-exercise__modal'>
+        <motion.form className='edit-exercise' initial={{ y: 0 }} animate={{ y: 10 }}>
+          <h2>Edit Exercise</h2>
+          <hr className='divider' />
+          {dateField}
+          {timeField}
+          {durationField}
+          {distanceField}
+          {noteField}
+          {buttonsContainer}
+        </motion.form>
+      </aside>,
+      document.body
+    );
+  } else {
+    return null;
+  }
+};
