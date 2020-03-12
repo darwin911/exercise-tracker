298e175e93d32aee9c4b60667b44bdeb29f6c975
Darwin Smith <darwin911@gmail.com>
2020-03-12 13:34:12 -0400

    refactors smaller components

--- client/web-client/src/components/home/Exercise.js
+++ client/web-client/src/components/home/Exercise.js
@@ -24,46 +24,70 @@ export const Exercise = ({ exercise }) => {
     dispatch({ type: TOGGLE_MODAL });
   };
 
+  const ExerciseDuration = () => (
+    <p className='exercise__duration'>
+      {duration} <span>mins</span>
+    </p>
+  );
+
+  const ExerciseType = () => <p className='exercise__type'>{type}</p>;
+
+  const ExerciseNote = () => {
+    if (!note) return null;
+    return (
+      <p className='exercise__note'>
+        <span role='img' aria-label='Note'>
+          📝
+        </span>{' '}
+        {note}
+      </p>
+    );
+  };
+
+  const ExerciseDistance = () => {
+    if (!distance) return null;
+    return (
+      <p className='exercise__distance'>
+        {distance} <span>miles</span>
+      </p>
+    );
+  };
+
+  const ExerciseDeleteButton = () => (
+    <button className='btn delete' onClick={() => handleDelete(id)} disabled={deleting}>
+      {deleting ? (
+        <div className='loader' />
+      ) : (
+        <img className='cancel-svg' src={cancel} alt='Cancel Icon' />
+      )}
+    </button>
+  );
+
+  const ExerciseEditButton = () => (
+    <Link className='btn edit' onClick={() => toggleEdit()} to={`/home/edit/${id}`}>
+      ✎
+    </Link>
+  );
+
   return (
     <motion.div
       positionTransition
       className={`exercise ${type.toLowerCase()}`}
-      initial={{ y: -10, transformOrigin: 'center', opacity: 0.35 }}
+      initial={{ y: -10, opacity: 0.35 }}
       animate={{ y: 0, opacity: 1 }}
       exit={{ y: -10, opacity: 0.35 }}>
       <div className='exercise__left-container'>
         <p className='exercise__day-of-week'>{moment(date).format('dddd')}</p>
         <p className='exercise__date'>{moment(date).format('MMM D')}</p>
         <p className='exercise__time'>{moment(time, 'Hmm').format('h:mm a')}</p>
-        <p className='exercise__duration'>
-          {duration} <span>mins</span>
-        </p>
-        <p className='exercise__type'>{type}</p>
-        {note && (
-          <p className='exercise__note'>
-            <span role='img' aria-label='Note'>
-              📝
-            </span>{' '}
-            {note}
-          </p>
-        )}
-        {distance && (
-          <p className='exercise__distance'>
-            {distance} <span>miles</span>
-          </p>
-        )}
+        <ExerciseDuration duration={duration} />
+        <ExerciseType type={type} />
+        <ExerciseNote note={note} />
+        <ExerciseDistance distance={distance} />
       </div>
       <div className='exercise__right-container'>
-        <button className='btn delete' onClick={() => handleDelete(id)} disabled={deleting}>
-          {deleting ? (
-            <div className='loader' />
-          ) : (
-            <img className='cancel-svg' src={cancel} alt='Cancel Icon' />
-          )}
-        </button>
-        <Link className='btn edit' onClick={() => toggleEdit()} to={`/home/edit/${exercise.id}`}>
-          ✎
-        </Link>
+        <ExerciseDeleteButton id={id} deleting={deleting} handleDelete={handleDelete} />
+        <ExerciseEditButton id={id} toggleEdit={toggleEdit} />
       </div>
     </motion.div>
   );
