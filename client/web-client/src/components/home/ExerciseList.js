563a432415a82d0200081ddeaac03f738e9ca2d6
Darwin Smith <darwin911@gmail.com>
2020-03-13 16:23:45 -0400

    refactor WIP

--- client/web-client/src/components/home/ExerciseList.js
+++ client/web-client/src/components/home/ExerciseList.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { Exercise } from './Exercise';
 import { AnimatePresence } from 'framer-motion';
 import { motion } from 'framer-motion';
+import { AuthContext } from '../../Store';
 
-export const ExerciseList = ({ exercises }) => {
-  if (!exercises.length)
-    return (
-      <div>
-        <h3>Sorry</h3>
-        <p>No exercises found.</p>
-      </div>
-    );
+const filterExercisesByType = (value = 'All', exerciseCollection) => {
+  if (value === 'All') return exerciseCollection;
+  return exerciseCollection.filter(ex => ex.type === value);
+};
+
+export const ExerciseList = () => {
+  const [{ filter, exercises }, dispatch] = useContext(AuthContext);
+
+  const [filteredExercises, setFilteredExercises] = useState(exercises);
+
+  useEffect(() => {
+    if (filter === 'All') {
+      setFilteredExercises(exercises);
+      return;
+    }
+
+    setFilteredExercises(filterExercisesByType(filter, exercises));
+  }, [filter, exercises]);
+
+  let renderExerciseList = filteredExercises.length ? filteredExercises : exercises;
+
+  if (!filteredExercises.length) return <EmptyFilterResult />;
+
   return (
     <motion.div className='exercises__container'>
       <AnimatePresence>
-        {exercises.map(exercise => (
+        {renderExerciseList.map(exercise => (
           <Exercise key={exercise.id} exercise={exercise} />
         ))}
       </AnimatePresence>
     </motion.div>
   );
 };
+
+const EmptyFilterResult = () => (
+  <div>
+    <h3>Sorry</h3>
+    <p>No exercises found.</p>
+  </div>
+);
