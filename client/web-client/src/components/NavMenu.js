0f012ca4426b55a0c4824a88e9be4062be8a98bd
Darwin Smith <darwin911@gmail.com>
2020-03-14 09:32:26 -0400

    adds transition to navMenu

--- client/web-client/src/components/NavMenu.js
+++ client/web-client/src/components/NavMenu.js
@@ -2,9 +2,19 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../Store';
 import { Link } from 'react-router-dom';
 import { LOGOUT } from '../constants';
+import { motion } from 'framer-motion';
 
-const style = {
-  width: '100%',
+const Span = () => (
+  <span
+    style={{
+      marginBottom: '5vh',
+    }}></span>
+);
+
+const spring = {
+  type: 'spring',
+  damping: 50,
+  stiffness: 600,
 };
 
 export const NavMenu = () => {
@@ -16,15 +26,21 @@ export const NavMenu = () => {
   };
 
   return (
-    <div className='nav-menu' style={style}>
+    <motion.div
+      className='nav-menu'
+      transition={spring}
+      style={{ width: '100%' }}
+      initial={{ y: '-100%', opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: 0, opacity: 0 }}>
       <Link to='/login' onClick={() => handleLogout()} className='btn logout'>
         Logout
       </Link>
-      <span style={{ marginBottom: 20 }}></span>
+      <Span />
       <Link to={`/profile/${user.id}`}>Profile</Link>
-      <span style={{ marginBottom: 20 }}></span>
+      <Span />
       <Link to='/home'>Home</Link>
-      <span style={{ marginBottom: 20 }}></span>
-    </div>
+      <Span />
+    </motion.div>
   );
 };
