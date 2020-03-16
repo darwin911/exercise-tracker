48d140cdf46f2f93506b69fef08faab71e4a4098
Darwin Smith <darwin911@gmail.com>
2020-03-15 20:23:01 -0400

    updateese filter list with activity types

--- client/web-client/src/components/FilterExercises.js
+++ client/web-client/src/components/FilterExercises.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { CONSTANTS } from '../constants';
+import { CONSTANTS, ACTIVITY_TYPES } from '../constants';
 import { AuthContext } from '../Store';
 const { EXERCISE_TYPES } = CONSTANTS;
+const activityTypes = Object.values(ACTIVITY_TYPES).map(type => type.title);
 
 const exerciseTypeValues = Object.values(EXERCISE_TYPES);
 
@@ -15,12 +16,20 @@ export const FilterExercises = () => {
     <aside className='filter__wrapper'>
       <label htmlFor='filter'>Filter: </label>
       <select id='filter' className='filter' value={filter} onChange={e => handleSetFilter(e)}>
-        <option value='All'>All</option>
+        <option value='ALL'>All Exercises</option>
+        <option disabled>--------</option>
         {exerciseTypeValues.map(type => (
           <option key={type} value={type}>
             {type}
           </option>
         ))}
+        <option disabled>--------</option>
+        {activityTypes.map(type => (
+          <option key={type} value={type}>
+            {type}
+          </option>
+        ))}
+        <option value=''></option>
       </select>
     </aside>
   );
