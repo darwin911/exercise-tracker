52eb10623d098edb7fda2e34544c22fd223fdd3f
Darwin Smith <darwin911@gmail.com>
2020-03-15 20:23:18 -0400

    removes <hr />

--- client/web-client/src/components/home/ExercisesSummary.js
+++ client/web-client/src/components/home/ExercisesSummary.js
@@ -2,19 +2,16 @@ import React from 'react';
 
 export const ExercisesSummary = ({ minutes, count, miles }) => {
   return (
-    <>
-      <div className='user-exercises__summary'>
-        <p className='user-exercises__summary__total-mins'>
-          {minutes} <span>Mins</span>
-        </p>
-        <p className='user-exercises__summary__total-exercises'>
-          {count} <span>Exercises</span>
-        </p>
-        <p className='user-exercises__summary__total-miles'>
-          {miles} <span>Miles</span>
-        </p>
-      </div>
-      <hr />
-    </>
+    <div className='user-exercises__summary'>
+      <p className='user-exercises__summary__total-mins'>
+        {minutes} <span>Mins</span>
+      </p>
+      <p className='user-exercises__summary__total-exercises'>
+        {count} <span>Exercises</span>
+      </p>
+      <p className='user-exercises__summary__total-miles'>
+        {miles} <span>Miles</span>
+      </p>
+    </div>
   );
 };
