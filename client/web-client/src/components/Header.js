2bf000a2231756bc2108c205a2ba0fe8067b0f47
Darwin Smith <darwin911@gmail.com>
2020-03-17 16:05:05 -0400

    creates MainHeading component

--- client/web-client/src/components/Header.js
+++ client/web-client/src/components/Header.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { NavMenu } from './NavMenu';
+import { MainHeading } from './MainHeading';
 
 export const Header = ({ isOpen, setMenuOpen }) => {
   return (
     <header className={`header`}>
       <nav>
-        <h1 className='main-heading'>
-          E<span className='hide-sm'>xercise </span>
-          <b>
-            T<span className='hide-sm'>racker</span>
-          </b>
-        </h1>
+        <MainHeading />
         <button className='hamburger-btn' onClick={() => setMenuOpen(val => !val)}>
           <span />
         </button>
---
+++ client/web-client/src/components/MainHeading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const MainHeading = () => (
+  <h1 className='main-heading'>
+    E<span className='hide-sm'>xercise </span>
+    <b>
+      T<span className='hide-sm'>racker</span>
+    </b>
+  </h1>
+);
