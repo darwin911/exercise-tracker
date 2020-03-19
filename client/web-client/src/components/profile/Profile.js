85e722648c39b240490c2490ca505ac636fc3f6d
Darwin Smith <darwin911@gmail.com>
2020-03-18 23:29:52 -0400

    WIP Profile

--- client/web-client/src/components/profile/Profile.js
+++ client/web-client/src/components/profile/Profile.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Store';
+import { Loader } from '../Loader';
+import { Header } from '../Header';
 
-export const Profile = () => {
+export const Profile = ({ isOpen, setMenuOpen }) => {
+  const [{ user }] = useContext(AuthContext);
+  if (!user) {
+    return <Loader size={4} />;
+  }
   return (
-    <div>
-      <p>Profile</p>
-    </div>
+    <main className='container'>
+      <Header isOpen={isOpen} setMenuOpen={setMenuOpen} />
+      <div className='profile'>
+        <h1>Profile</h1>
+        <h3>{user.username}</h3>
+        <h4>Id: {user.id}</h4>
+        <img src='https://picsum.photos/100' alt='img' />
+      </div>
+    </main>
   );
 };
