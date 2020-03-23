765db82a8043818887690cb10ffe53b2d0bd0ebe
Darwin Smith <darwin911@gmail.com>
2020-03-23 18:04:15 -0400

    refactor: takes Header outside of profile container

--- client/web-client/src/components/profile/Profile.js
+++ client/web-client/src/components/profile/Profile.js
@@ -1,22 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { AuthContext } from '../../Store';
 import { Loader } from '../Loader';
 import { Header } from '../Header';
 
 export const Profile = ({ isOpen, setMenuOpen }) => {
   const [{ user }] = useContext(AuthContext);
+  const [weight] = useState(175);
   if (!user) {
     return <Loader size={4} />;
   }
   return (
-    <main className='container'>
+    <>
       <Header isOpen={isOpen} setMenuOpen={setMenuOpen} />
-      <div className='profile'>
-        <h1>Profile</h1>
-        <h3>{user.username}</h3>
-        <h4>Id: {user.id}</h4>
-        <img src='https://picsum.photos/100' alt='img' />
-      </div>
-    </main>
+      <main className='container'>
+        <div className='profile'>
+          <h1>Profile</h1>
+          <h3>{user.username}</h3>
+          <h4>Id: {user.id}</h4>
+          <img src='https://picsum.photos/100' alt='img' />
+          <p>email: {user.email}</p>
+          <p>weight: {weight}lbs</p>
+        </div>
+      </main>
+    </>
   );
 };
