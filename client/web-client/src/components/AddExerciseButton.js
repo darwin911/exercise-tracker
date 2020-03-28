cc223bffe4dd0382704e51925a9b7ec0ed3c1b36
Darwin Smith <darwin911@gmail.com>
2020-03-28 14:55:03 -0400

    removes unsued code

--- client/web-client/src/components/AddExerciseButton.js
+++ client/web-client/src/components/AddExerciseButton.js
@@ -1,15 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { CONSTANTS } from '../constants';
-import { AuthContext } from '../Store';
 import { AddSVG } from './AddSVG';
-const { TOGGLE_MODAL } = CONSTANTS;
 
 export const AddExerciseButton = () => {
-  const dispatch = useContext(AuthContext)[1];
   const history = useHistory();
   const toggleModal = () => {
-    dispatch({ type: TOGGLE_MODAL });
     history.push('/home/add');
   };
   return (
