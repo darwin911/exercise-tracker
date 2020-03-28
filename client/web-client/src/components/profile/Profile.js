01107cea497b983ec2b9be36ffa16c9762c8a922
Darwin Smith <darwin911@gmail.com>
2020-03-28 13:12:13 -0400

    separates ProfileCard

--- client/web-client/src/components/profile/Profile.js
+++ client/web-client/src/components/profile/Profile.js
@@ -5,30 +5,40 @@ import { Header } from '../Header';
 
 export const Profile = ({ isOpen, setMenuOpen }) => {
   const [{ user }] = useContext(AuthContext);
-  const [weight] = useState(175);
-  if (!user) {
-    return <Loader size={4} />;
-  }
-  const size = 56;
+
+  if (!user) return <Loader size={4} />;
+
   return (
     <>
       <Header isOpen={isOpen} setMenuOpen={setMenuOpen} />
       <main className='container'>
         <div className='profile'>
           <h1>Profile</h1>
-          <div className='profile__card'>
-            <h3>Username: {user.username}</h3>
-            <img
-              src={`https://picsum.photos/${size}`}
-              alt='img'
-              style={{ minWidth: size, minHeight: size }}
-            />
-            <p>Email: {user.email}</p>
-            <p>Weight: {weight}lbs</p>
-            {user.unitSystem && <p>Units: {user.unitSystem}</p>}
-          </div>
+          <ProfileCard />
         </div>
       </main>
     </>
+  );
+};
+
+const ProfileCard = () => {
+  const [{ user }] = useContext(AuthContext);
+  const [weight] = useState(175);
+  const size = 48;
+  return (
+    <div className='profile__card'>
+      <header className='profile__card__header'>
+        <h3>{user.username}</h3>
+        <img
+          src={`https://picsum.photos/${size}`}
+          alt='img'
+          style={{ minWidth: size, minHeight: size }}
+        />
+      </header>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <p>Weight: {weight}lbs</p>
+      {user.unitSystem && <p>Units: {user.unitSystem}</p>}
+    </div>
   );
 };
