1ee301b22e722c85193695c47d0915751b2ca2f0
Darwin Smith <darwin911@gmail.com>
2020-03-29 11:37:59 -0400

    abstracts forms into separate components

--- client/web-client/src/components/auth/Auth.js
+++ client/web-client/src/components/auth/Auth.js
@@ -1,111 +1,15 @@
-import React, { useContext } from 'react';
-import { AuthLink, FormField, MainHeading } from './index';
-import { Route, useHistory } from 'react-router-dom';
-import { loginUser, registerUser } from '../../helper';
-import { AuthContext } from '../../Store';
-import { CONSTANTS } from '../../constants';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-
-const { SET_USER } = CONSTANTS;
-
-const validation = Yup.object({
-  name: Yup.string().max(2, 'Must be 255 characters or less'),
-  username: Yup.string().min(6, 'Must be 6 characters or more'),
-  email: Yup.string()
-    .email('Invalid email address')
-    .required('Required'),
-  password: Yup.string()
-    .min(4, 'Must be 4 characters or more')
-    .required('Required'),
-});
+import React from 'react';
+import { MainHeading, RegisterForm, LoginForm } from './index';
+import { Route } from 'react-router-dom';
 
 export const Auth = () => {
-  const [state, dispatch] = useContext(AuthContext);
-  const history = useHistory();
-  const heading = window.location.pathname.includes('login') ? 'Login' : 'Register';
-
-  const handleSubmit = async values => {
-    const { email, username, password } = values;
-    const { pathname } = window.location;
-    let authenticatedUser;
-    if (pathname.includes('/auth/login')) {
-      authenticatedUser = await loginUser({ email, password });
-    } else if (pathname === '/auth/register') {
-      authenticatedUser = await registerUser({ username, email, password });
-    }
-    if (authenticatedUser) {
-      if (authenticatedUser.error) {
-        alert(authenticatedUser.error);
-        return;
-      }
-      dispatch({ type: SET_USER, payload: authenticatedUser });
-      localStorage.setItem('token', authenticatedUser.token);
-      history.push('/home');
-    }
-  };
   return (
     <main className='auth container'>
       <header>
         <MainHeading />
       </header>
-      <Formik
-        initialValues={{
-          name: '',
-          username: '',
-          email: '',
-          password: '',
-          error: '',
-        }}
-        validationSchema={validation}
-        onSubmit={handleSubmit}>
-        {({ values, errors, touched, handleSubmit, handleChange, handleBlur, isSubmitting }) => (
-          <form onSubmit={handleSubmit}>
-            <h2>{heading}</h2>
-            <br />
-            <Route
-              path='/auth/register'
-              render={() => (
-                <React.Fragment>
-                  <FormField
-                    inputType='name'
-                    value={values.name}
-                    handleChange={handleChange}
-                    handleBlur={handleBlur}
-                    disabled={isSubmitting}
-                  />
-                  <FormField
-                    inputType='username'
-                    value={values.username}
-                    handleChange={handleChange}
-                    handleBlur={handleBlur}
-                    disabled={isSubmitting}
-                  />
-                </React.Fragment>
-              )}
-            />
-            <FormField
-              inputType='email'
-              value={values.email}
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-              error={touched.email && errors.email}
-              disabled={isSubmitting}
-            />
-            <FormField
-              inputType='password'
-              value={values.password}
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-              error={touched.password && errors.password}
-              disabled={isSubmitting}
-            />
-            <button type='submit'>{isSubmitting ? <div className='loader' /> : 'Submit'}</button>
-            <Route path='/auth/login' render={() => <AuthLink path='register' />} />
-            <Route path='/auth/register' render={() => <AuthLink path='login' />} />
-          </form>
-        )}
-      </Formik>
+      <Route path='/auth/login' component={LoginForm} />
+      <Route path='/auth/register' component={RegisterForm} />
     </main>
   );
 };
