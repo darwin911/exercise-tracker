6ab3759e717923a8ca372aff44150a6ca9a18c70
Darwin Smith <darwin911@gmail.com>
2020-03-29 11:38:17 -0400

    renders text conditionally for AuthLink

--- client/web-client/src/components/auth/AuthLink.js
+++ client/web-client/src/components/auth/AuthLink.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const AuthLink = ({ path }) => {
+  const isLogin = path.includes('/login');
   return (
     <p>
-      Don't have an account? <Link to={`/auth/${path}`}>{path}</Link>
+      {isLogin ? "Don't " : 'Already '}
+      have an account?{' '}
+      <Link to={`/auth/${path}`} style={{ textTransform: 'capitalize' }}>
+        {path}
+      </Link>
     </p>
   );
 };
