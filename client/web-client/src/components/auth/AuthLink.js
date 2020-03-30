b97c4f9e18b46cfd67104552ca16edc9d52bd853
Darwin Smith <darwin911@gmail.com>
2020-03-30 15:48:00 -0400

    fix: was not toggling

--- client/web-client/src/components/auth/AuthLink.js
+++ client/web-client/src/components/auth/AuthLink.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const AuthLink = ({ path }) => {
-  const isLogin = path.includes('/login');
+  const isLogin = window.location.pathname.includes('/login');
   return (
     <p>
       {isLogin ? "Don't " : 'Already '}
