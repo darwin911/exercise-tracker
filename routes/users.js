8d650d565f0842b32af06f29d4c2b46db2522a41
Darwin Smith <darwin911@gmail.com>
2020-04-04 18:07:27 -0400

    update verify / creates delete route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
+let Exercise = require('../models/exercise.model');
 const { verify } = require('../auth');
 
 router.route('/').get(async (req, res) => {
   try {
     let users = await User.find();
-    users = await Promise.all(users.map(user => user.toClient()));
+    users = await Promise.all(users.map((user) => user.toClient()));
     res.json({ users });
   } catch (error) {
     console.log(error);
@@ -15,8 +16,12 @@ router.route('/').get(async (req, res) => {
 router.route('/:id').get(async (req, res) => {
   try {
     let user = await User.findById(req.params.id);
-    user = await user.toClient();
-    res.json(user);
+    if (user) {
+      user = await user.toClient();
+      return res.json(user);
+    } else {
+      return res.status(404).json({ error: { message: 'User not found', code: 404 } });
+    }
   } catch (error) {
     console.log(error);
   }
@@ -29,13 +34,13 @@ router.route('/login').post(async (req, res) => {
 
     if (!user) {
       return res.status(401).json({
-        error: 'Invalid Credentials: No account with this email has been found'
+        error: 'Invalid Credentials: No account with this email has been found',
       });
     } else {
       const isAuthenticated = await user.isValidPassword(password);
       if (!isAuthenticated) {
         return res.status(401).json({
-          error: 'Invalid Credentials: Email and password are not correct'
+          error: 'Invalid Credentials: Email and password are not correct',
         });
       }
       const userData = await user.toAuthJSON();
@@ -66,10 +71,33 @@ router.route('/register').post(async (req, res) => {
 
 router.route('/verify').post(async (req, res) => {
   try {
-    const { id } = await verify(req.body.token);
-    const user = await User.findById(id);
-    const userData = await user.toClient();
-    res.json(userData);
+    const tokenData = await verify(req.body.token);
+    if (tokenData) {
+      const user = await User.findById(tokenData.id);
+      if (user) {
+        const userData = await user.toClient();
+        return res.json(userData);
+      } else {
+        return res.status(401).json({ errror: { message: 'Unauthorized', code: 401 } }); // 401 // Unauthorized
+      }
+    } else {
+      return res.status(401).json({ errror: { message: 'Unauthorized', code: 401 } });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.route('/:id').delete(async (req, res) => {
+  try {
+    const { id } = req.params;
+    let user = await User.findById(id);
+    if (user) {
+      await Exercise.deleteMany({ userId: id });
+      user = await User.findByIdAndDelete(id);
+      return res.status(204).json({ user }); // 204 : No Content
+    }
+    return res.status(404).json({ error: { message: 'User Not Found', code: 404 } });
   } catch (error) {
     console.log(error);
   }
