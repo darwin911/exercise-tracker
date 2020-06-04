2c6ca1cfe59e7aeb793d97b4277632b3871883bf
Darwin Smith <darwin911@gmail.com>
2020-06-04 18:35:20 -0400

    creates accept-friend, decline-friend, and decline-friend route

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,7 @@ router.route('/').get(async (req, res) => {
   }
 });
 
+// Get User
 router.route('/:id').get(async (req, res) => {
   try {
     let user = await User.findById(req.params.id);
@@ -39,6 +40,123 @@ router.route('/:id/update').put(async (req, res) => {
       user.save();
       const updatedUser = user.toClient();
       return res.json(updatedUser);
+    } else {
+      return res.status(404).json({ error: { message: 'User not found', code: 404 } });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.route('/:id/add-friend').post(async (req, res) => {
+  const { targetId } = req.body;
+  const { id } = req.params;
+  try {
+    let user = await User.findById(id);
+    if (user) {
+      if (targetId) {
+        let targetUser = await User.findById(targetId);
+
+        console.log(`\n${user.username} is sending friend request to ${targetUser.username}\n`);
+
+        const friendReqs = targetUser.friendRequests;
+        if (!friendReqs.includes(id)) {
+          friendReqs.push(id);
+          targetUser.save();
+          return res.json({
+            success: true,
+            message: `Friend request for user with Id ${targetId}`,
+          });
+        } else {
+          return res.status(409).json({
+            error: { message: 'Already Requested Friend. Pending Confirmation from target user' },
+          });
+        }
+      }
+    } else {
+      return res.status(404).json({ error: { message: 'User not found', code: 404 } });
+    }
+  } catch (error) {
+    console.error(error);
+    return { error: 'Already Requested Friend. Pending Confirmation from target user' };
+  }
+});
+
+router.route('/:id/accept-friend').post(async (req, res) => {
+  const { targetId } = req.body;
+  const { id } = req.params;
+  try {
+    let user = await User.findById(id);
+    let targetUser = await User.findById(targetId);
+    if (user && targetUser) {
+      const requestExists = user.friendRequests.includes(targetId);
+      if (requestExists) {
+        // remove from friendRequests
+        user.friendRequests.pull(targetId);
+        // add to friends array
+        user.friends.push(targetId);
+        targetUser.friends.push(id);
+        const updatedUser = user.toClient();
+        const updateTargetUser = targetUser.toClient();
+        user.save();
+        targetUser.save();
+        // return updated user object with update friends and friendReqs
+        return res.json(updatedUser);
+      }
+    } else {
+      return res.status(500).json({ error: "Couldn't process accept friend" });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.route('/:id/decline-friend/:targetId').delete(async (req, res) => {
+  const { id, targetId } = req.params;
+  try {
+    let user = await User.findById(id);
+    if (user) {
+      if (user.friendRequests) {
+        const index = user.friendRequests.indexOf(targetId);
+        const exists = index !== -1;
+        if (exists) {
+          user.friendRequests.pull(targetId);
+          user.save();
+          return res.status(200).json({
+            success: true,
+            message: `Friend request for user with Id ${targetId} has been declined successfully.`,
+          });
+        }
+      }
+    } else {
+      return res.status(404).json({ error: { message: 'User not found', code: 404 } });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.route('/:id/remove-friend/:targetId').delete(async (req, res) => {
+  const { id, targetId } = req.params;
+  try {
+    let user = await User.findById(id);
+    let targetUser = await User.findById(targetId);
+    if (user) {
+      if (user.friends) {
+        const index = user.friends.indexOf(targetId);
+        const exists = index !== -1;
+        if (exists) {
+          user.friends.pull(targetId);
+          targetUser.friends.pull(id);
+          user.save();
+          targetUser.save();
+          return res.status(200).json({
+            success: true,
+            updatedUser: user.toClient(),
+            message: `Friend connection with user Id ${targetId} has been removed.`,
+          });
+        }
+      }
     } else {
       return res.status(404).json({ error: { message: 'User not found', code: 404 } });
     }
