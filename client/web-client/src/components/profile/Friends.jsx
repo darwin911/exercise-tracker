3dfd3b281b75f85392f7b2bebe0afcda1856cb62
Darwin Smith <darwin911@gmail.com>
2020-06-07 10:25:12 -0400

    refactor into separate components

--- client/web-client/src/components/profile/Friends.jsx
+++ client/web-client/src/components/profile/Friends.jsx
@@ -1,91 +1,15 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../Store';
-import { CONSTANTS } from '../../constants';
-import { acceptFriendRequest, declineFriendRequest, removeFriend } from '../../helper';
+import React from 'react';
 import { FriendSearch } from './FriendSearch';
-
-const { DECLINE_FRIEND_REQUEST, SET_USER } = CONSTANTS;
+import { FriendList } from './FriendList';
+import { FriendRequests } from './FriendRequests';
 
 export const Friends = () => {
-  const [state] = useContext(AuthContext);
-  const { filteredFriendSearch } = state;
-
   return (
     <section className='profile__component friends'>
+      <h2>Friends</h2>
+      <FriendList />
       <FriendRequests />
-      <FriendSearch userList={filteredFriendSearch} />
+      <FriendSearch />
     </section>
-  );
-};
-
-const FriendRequests = () => {
-  const [{ user }, dispatch] = useContext(AuthContext);
-  const { friendRequests, friends } = user;
-  console.log(friendRequests);
-  const friendRequestsPending = user.friendRequests ? user.friendRequests.length : 0;
-
-  const handleAcceptFriendRequest = async (targetId) => {
-    const updatedUser = await acceptFriendRequest(user.id, { targetId });
-    dispatch({ type: SET_USER, payload: updatedUser });
-  };
-
-  const handleDeclineFriendRequest = async (targetId) => {
-    const response = await declineFriendRequest(user.id, targetId);
-    if (response.success) {
-      dispatch({ type: DECLINE_FRIEND_REQUEST, payload: targetId });
-      alert(response.message);
-    }
-  };
-
-  const handleRemoveFriend = async (targetId) => {
-    const response = await removeFriend(user.id, targetId);
-    if (response && response.success) {
-      dispatch({ type: SET_USER, payload: response.updatedUser });
-      alert(response.message);
-    }
-  };
-
-  const FriendRequests = () => {
-    if (!friendRequests) return null;
-    return (
-      <div className='friend-requests'>
-        {friendRequests.map((userId) => (
-          <div className='friend-request' key={userId}>
-            <p>Id: {userId}</p>
-            <button onClick={() => handleAcceptFriendRequest(userId)}>Accept</button>
-            <button onClick={() => handleDeclineFriendRequest(userId)}>Decline</button>
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  const FriendList = ({ friends }) => {
-    const friendList =
-      friends &&
-      friends.map((friend) => (
-        <div key={friend}>
-          <p>{friend}</p>
-          <button onClick={() => handleRemoveFriend(friend)}>Remove Friend</button>
-        </div>
-      ));
-
-    return (
-      <div className='friend-list'>
-        <p>You have {friends ? friends.length : 0} friends</p>
-        {friendList}
-        <br />
-      </div>
-    );
-  };
-
-  return (
-    <div>
-      <p>You have {friendRequestsPending} friend requests</p>
-      <br />
-      <FriendRequests />
-      <br />
-      <FriendList friends={friends} />
-    </div>
   );
 };
