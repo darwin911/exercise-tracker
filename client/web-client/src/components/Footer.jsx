45485a7eca1c091969e9d7bf0eaac85280896bf9
Darwin Smith <darwin911@gmail.com>
2020-06-27 11:32:22 -0400

    updates footer markup

--- client/web-client/src/components/Footer.jsx
+++ client/web-client/src/components/Footer.jsx
@@ -4,7 +4,9 @@ export const Footer = () => (
   <footer className='footer'>
     <div className='container'>
       <br />
-      <strong>Created by &copy; Darwin Smith</strong>
+      <strong>
+        Created by &copy; <span>Darwin Smith</span>
+      </strong>
       <span> {new Date().getFullYear()}</span>
       <br />
     </div>
