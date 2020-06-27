cce3ef91cffc2bc158c458d331463b8b52698a37
Darwin Smith <darwin911@gmail.com>
2020-06-27 14:55:59 -0400

    Adds initial chart with dummy data

--- client/web-client/src/components/profile/WeightTracker.jsx
+++ client/web-client/src/components/profile/WeightTracker.jsx
@@ -1,4 +1,20 @@
 import React from 'react';
+import { LineChart, XAxis, YAxis, CartesianGrid, Line } from 'recharts';
+
+const data = [
+  {
+    weight: 100,
+    day: 'Monday',
+  },
+  {
+    weight: 110,
+    day: 'Tuesday',
+  },
+  {
+    weight: 115,
+    day: 'Wednesday',
+  },
+];
 
 export const WeightTracker = () => {
   return (
@@ -8,7 +24,12 @@ export const WeightTracker = () => {
       </header>
       <article className='profile__component__body'>
         <h3>Weight Over Time</h3>
-        <p>[insert chart]</p>
+        <LineChart width={500} height={300} data={data}>
+          <XAxis dataKey='day' />
+          <YAxis />
+          <CartesianGrid stroke='#eee' strokeDasharray='3 3' />
+          <Line type='monotone' dataKey='weight' stroke='#8884d8' />
+        </LineChart>
       </article>
       <footer className='profile__component__footer'>
         <p>Footer</p>
