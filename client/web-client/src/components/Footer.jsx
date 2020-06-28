0b277dd9af2e391f0456d2d2d1bd0e4cb6a067df
Darwin Smith <darwin911@gmail.com>
2020-06-28 11:33:31 -0400

    updates footer content

--- client/web-client/src/components/Footer.jsx
+++ client/web-client/src/components/Footer.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 export const Footer = () => (
   <footer className='footer'>
     <div className='container'>
-      <br />
-      <strong>
-        Created by &copy; <span>Darwin Smith</span>
-      </strong>
-      <span> {new Date().getFullYear()}</span>
-      <br />
+      <p className='footer-copy'>
+        Created by
+        <a href='https://www.darwinpsmith.com' target='_blank' rel='noreferrer noopener'>
+          {' '}
+          <span> &copy; Darwin Smith {new Date().getFullYear()}</span>
+        </a>
+        <a
+          href='https://www.github.com/darwin911'
+          className='github-icon'
+          target='_blank'
+          rel='noreferrer noopener'>
+          <img src={process.env.PUBLIC_URL + '/images/github-icon.svg'} alt='Github' />
+        </a>
+      </p>
     </div>
   </footer>
 );
