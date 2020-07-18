7cfea00952f6fc236ae96d6b63c89ef4ba87064a
Darwin Smith <darwin911@gmail.com>
2020-07-18 13:37:47 -0400

    moves filterExercisesByDate to /util

--- client/web-client/src/util/exercises-helper.js
+++ client/web-client/src/util/exercises-helper.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export const getTotalPushups = (exercisesArray) =>
   exercisesArray.reduce(
     (acc, item) => (item.repetitions ? acc + Number(item.repetitions) : acc),
@@ -22,4 +24,36 @@ export const getTotalMiles = (exercisesArray) => {
       exercisesArray.reduce((acc, item) => (item.distance ? acc + item.distance : acc), 0) * 100
     ) / 100
   );
+};
+
+export const filterExercisesByDate = (exercises, dateFilter) => {
+  let thirtyDaysAgo = moment().subtract(30, 'day').format('YYYY-MM-DD');
+  let sevenDaysAgo = moment().subtract(7, 'day').format('YYYY-MM-DD');
+  let threeDaysAgo = moment().subtract(3, 'day').format('YYYY-MM-DD');
+  let yesterday = moment().subtract(1, 'day').format('YYYY-MM-DD');
+  let today = moment().format('YYYY-MM-DD');
+  switch (dateFilter) {
+    case 'Last 30 Days':
+      return exercises.filter((exercise) =>
+        moment(exercise.date, 'YYYY-MM-DD').isSameOrAfter(thirtyDaysAgo)
+      );
+    case 'Last 7 Days':
+      return exercises.filter((exercise) =>
+        moment(exercise.date, 'YYYY-MM-DD').isSameOrAfter(sevenDaysAgo)
+      );
+    case 'Last 3 Days':
+      return exercises.filter((exercise) =>
+        moment(exercise.date, 'YYYY-MM-DD').isSameOrAfter(threeDaysAgo)
+      );
+    case 'Yesterday':
+      return exercises.filter((exercise) =>
+        moment(exercise.date, 'YYYY-MM-DD').isSameOrAfter(yesterday)
+      );
+    case 'Today':
+      return exercises.filter((exercise) =>
+        moment(exercise.date, 'YYYY-MM-DD').isSameOrAfter(today)
+      );
+    default:
+      return exercises;
+  }
 };
