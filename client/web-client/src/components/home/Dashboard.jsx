b4ed4f1808f527b03f1cf18479438436052080af
Darwin Smith <darwin911@gmail.com>
2020-07-19 15:00:46 -0400

    adds heading to dashboard

--- client/web-client/src/components/home/Dashboard.jsx
+++ client/web-client/src/components/home/Dashboard.jsx
@@ -7,12 +7,15 @@ import { AppContext } from '../../Store';
 export const Dashboard = ({ data }) => {
   const [{ user }] = useContext(AppContext);
   return (
-    <aside className='dashboard'>
-      <ExercisesSummary username={user ? user.username : 'Guest'} {...data} />
-      <div className='filter__container'>
-        <FilterExercises />
-        <AddExerciseButton />
-      </div>
-    </aside>
+    <>
+      <aside className='dashboard'>
+        <h3 className='dashboard__heading'>Dashboard</h3>
+        <ExercisesSummary username={user ? user.username : 'Guest'} {...data} />
+        <div className='filter__container'>
+          <FilterExercises />
+          <AddExerciseButton />
+        </div>
+      </aside>
+    </>
   );
 };
