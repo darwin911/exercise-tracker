7fd1619ecb86bbfef92a3e58abf20b033431d71e
Darwin Smith <darwin911@gmail.com>
2021-08-15 16:32:36 -0400

    refactor Exercise component

--- client/web-client/src/components/home/Exercise.jsx
+++ client/web-client/src/components/home/Exercise.jsx
@@ -1,18 +1,22 @@
-import React, { useContext, useState } from 'react';
-import { deleteExercise } from '../../helper';
-import moment from 'moment';
-import { AppContext } from '../../Store';
 import { CONSTANTS, EXERCISE_TYPES, TRANSITIONS } from '../../constants';
-import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+
+import { AppContext } from '../../Store';
 import { CancelSVG } from '../shared/index';
+import { Link } from 'react-router-dom';
+import { deleteExercise } from '../../helper';
+import kebabCase from 'lodash/kebabCase';
+import moment from 'moment';
+import { motion } from 'framer-motion';
+
 const { REMOVE_EXERCISE, TOGGLE_MODAL } = CONSTANTS;
 
 export const Exercise = ({ exercise }) => {
   const dispatch = useContext(AppContext)[1];
 
   const [deleting, setDeleting] = useState(false);
-  const { id, note, duration, date, time, type, distance, repetitions } = exercise;
+  const { id, note, duration, date, time, type, distance, repetitions } =
+    exercise;
 
   const handleDelete = async (id) => {
     setDeleting(true);
@@ -82,40 +86,58 @@ export const Exercise = ({ exercise }) => {
   };
 
   const ExerciseDeleteButton = () => (
-    <button className='btn delete' onClick={() => handleDelete(id)} disabled={deleting}>
+    <button
+      className='btn delete'
+      onClick={() => handleDelete(id)}
+      disabled={deleting}>
       {deleting ? <div className='loader' /> : <CancelSVG />}
     </button>
   );
 
   const ExerciseEditButton = () => (
-    <Link className='btn edit' onClick={() => toggleEdit()} to={`/home/edit/${id}`}>
+    <Link
+      className='btn edit'
+      onClick={() => toggleEdit()}
+      to={`/home/edit/${id}`}>
       ✎
     </Link>
   );
 
-  const className = type.toLowerCase().replace(/ /g, '-');
+  const ExerciseDateAndTime = () => {
+    const exerciseDate = moment.utc(date).format('MMM D');
+    const exerciseTime = moment(time, 'Hmm').format('h:mm a');
+    return (
+      <>
+        <p className='exercise__day-of-week'>
+          {moment.utc(date).format('ddd')}
+        </p>
+        <p className='exercise__date'>{exerciseDate}</p>
+        <p className='exercise__time'>{exerciseTime}</p>
+      </>
+    );
+  };
 
   return (
     <motion.div
-      // this casuses flicker
-      // positionTransition
       layoutTransition={TRANSITIONS.SPRING}
-      className={`exercise ${className}`}
+      className={`exercise ${kebabCase(type)}`}
       initial={{ y: -10, opacity: 0.15 }}
       animate={{ y: 0, opacity: 1 }}
       exit={{ y: -5, opacity: 0.15 }}
       tabIndex={0}>
       <div className='exercise__left-container'>
-        <p className='exercise__day-of-week'>{moment.utc(date).format('dddd')}</p>
-        <p className='exercise__date'>{moment.utc(date).format('MMM D')}</p>
-        <p className='exercise__time'>{moment(time, 'Hmm').format('h:mm a')}</p>
+        <ExerciseDateAndTime />
         <ExerciseDuration />
         <ExerciseType />
         <ExerciseNote />
         <ExerciseDistance />
       </div>
       <div className='exercise__right-container'>
-        <ExerciseDeleteButton id={id} deleting={deleting} handleDelete={handleDelete} />
+        <ExerciseDeleteButton
+          id={id}
+          deleting={deleting}
+          handleDelete={handleDelete}
+        />
         <ExerciseEditButton id={id} toggleEdit={toggleEdit} />
       </div>
     </motion.div>
