02e18d92ea50a124a87dd1815be45ea7ed448f80
Darwin Smith <darwin911@gmail.com>
2022-09-30 19:02:21 +0200

    cleanup

--- client/web-client/src/components/home/UserExercises.jsx
+++ client/web-client/src/components/home/UserExercises.jsx
@@ -1,31 +1,33 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { AppContext } from '../../Store';
-import { ExerciseList, Dashboard } from './index';
-// import { Loader } from '../shared/index';
-import { ACTIVITY_TYPES } from '../../constants';
+import { Dashboard, ExerciseList } from "./index";
+import React, { useContext, useEffect, useState } from "react";
 import {
-  getTotalPushups,
-  getTotalMinutes,
-  getTotalMiles,
   filterExercisesByActivity,
-  filterExercisesByType,
   filterExercisesByDate,
-} from '../../util/exercises-helper';
+  filterExercisesByType,
+  getTotalMiles,
+  getTotalMinutes,
+  getTotalPushups,
+} from "../../util/exercises-helper";
+
+import { ACTIVITY_TYPES } from "../../constants";
+import { AppContext } from "../../Store";
 
 const activityTypes = Object.values(ACTIVITY_TYPES).map((type) => type.title);
 
 export const UserExercises = () => {
-  const { exercises, loading, filterByType, filterByDate } = useContext(AppContext)[0];
+  const { exercises, loading, filterByType, filterByDate } =
+    useContext(AppContext)[0];
   const [filteredExercises, setFilteredExercises] = useState(exercises);
 
   useEffect(() => {
-    if (filterByType === 'ALL' && filterByDate === 'All Time') {
+    if (filterByType === "ALL" && filterByDate === "All Time") {
       setFilteredExercises(exercises);
-    } else if (filterByType === 'ALL') {
+    } else if (filterByType === "ALL") {
       let filteredExercises = filterExercisesByDate(exercises, filterByDate);
       setFilteredExercises(filteredExercises);
     } else {
-      const isActivityType = activityTypes.indexOf(filterByType.replace(/_/g, ' ')) !== -1;
+      const isActivityType =
+        activityTypes.indexOf(filterByType.replace(/_/g, " ")) !== -1;
       let exercisesToUpdate = isActivityType
         ? filterExercisesByActivity(filterByType, exercises)
         : filterExercisesByType(filterByType, exercises);
@@ -51,7 +53,7 @@ export const UserExercises = () => {
   };
 
   return (
-    <section className='user-exercises'>
+    <section className="user-exercises">
       <Dashboard data={data} />
       <ExerciseList exercises={filteredExercises} />
     </section>
